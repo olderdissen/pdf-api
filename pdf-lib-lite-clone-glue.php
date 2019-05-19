d5d7f2debbc37ff39522a61a5b2db3c84aa9a1b9
olderdissen <50807808+olderdissen@users.noreply.github.com>
2019-05-19 21:35:32 +0200

    Create pdf-lib-lite-clone-glue.php

---
+++ pdf-lib-lite-clone-glue.php
@@ -0,0 +1,185 @@
+<?
+################################################################################
+# _pdf_glue_array ( array $array ) : string
+# returns $array as string.
+################################################################################
+
+function _pdf_glue_array($array, $optional = true)
+	{
+	$retval = array();
+
+	foreach($array as $value)
+		{
+		if($optional)
+			if(is_array($value))
+				$value = sprintf("[%s]", _pdf_glue_array($value));
+
+		$retval[] = sprintf("%s", $value);
+		}
+
+	return(implode(" ", $retval));
+	}
+
+################################################################################
+# _pdf_glue_dictionary ( array $dictionary ) : string
+# returns $dictionary as string.
+################################################################################
+
+function _pdf_glue_dictionary($dictionary, $optional = true)
+	{
+	$retval = array();
+
+	foreach($dictionary as $key => $value)
+		{
+		if($optional)
+			if(is_array($value))
+				$value = sprintf("<< %s >>", _pdf_glue_dictionary($value));
+
+		$retval[] = sprintf("%s %s", $key, $value);
+		}
+
+	return(implode(" ", $retval));
+	}
+
+################################################################################
+# _pdf_glue_document ( array $objects ) : string
+# returns $objects as string (pdf-format).
+################################################################################
+
+function _pdf_glue_document($objects, $optional = true)
+	{
+	################################################################################
+	# fix count
+	################################################################################
+
+	$objects[0]["dictionary"]["/Size"] = count($objects);
+
+	################################################################################
+	# header
+	################################################################################
+
+	$retval = array("%PDF-1.5");
+
+	################################################################################
+	# body
+	################################################################################
+
+	$offsets = array();
+
+	foreach($objects as $index => $object)
+		{
+		if($index == 0) # trailer
+			continue;
+
+		$offsets[$index] = strlen(implode("\n", $retval)) + 1;
+
+		$retval[] = _pdf_glue_object($object);
+		}
+
+	################################################################################
+	# xref
+	################################################################################
+
+	$startxref = strlen(implode("\n", $retval)) + 1;
+
+	################################################################################
+	# <entry> :: <start> <count> | <offset> <generation> <used>
+	################################################################################
+
+	if($optional)
+		ksort($objects);
+
+	$count = 0;
+	$start = 0;
+
+	$retval[] = sprintf("xref");
+
+	foreach($objects as $index => $object)
+		{
+		if($count == 0)
+			$start = $index;
+
+		$count++;
+
+		if(isset($objects[$index + 1]))
+			continue;
+
+		$retval[] = sprintf("%d %d", $start, $count);
+
+		foreach(range($start, $start + $count - 1) as $id)
+			if($id == 0)
+				$retval[] = sprintf("%010d %05d %s", 0, 65535, "f");
+			else
+				$retval[] = sprintf("%010d %05d %s", $offsets[$id], $objects[$id]["version"], "n");
+
+		$count = 0;
+		}
+
+	################################################################################
+	# trailer
+	################################################################################
+
+	$trailer = $objects[0]["dictionary"];
+
+	$retval[] = "trailer";
+	$retval[] = sprintf("<< %s >>", _pdf_glue_dictionary($trailer));
+
+	################################################################################
+	# startxref
+	################################################################################
+
+	$retval[] = "startxref";
+	$retval[] = $startxref;
+
+	################################################################################
+	# eof
+	################################################################################
+
+	$retval[] = "%%EOF";
+
+	################################################################################
+	# final pdf file
+	################################################################################
+
+	return(implode("\n", $retval));
+	}
+
+################################################################################
+# _pdf_glue_object ( array $object ) : string
+# returns $object as string (obj-format).
+################################################################################
+
+function _pdf_glue_object($object)
+	{
+	$retval = array();
+
+	$retval[] = sprintf("%d %d obj", $object["id"], $object["version"]);
+
+		if(isset($object["dictionary"]))
+			$retval[] = sprintf("<< %s >>", _pdf_glue_dictionary($object["dictionary"]));
+
+		if(isset($object["stream"]))
+			$retval[] = sprintf("stream\n%s\nendstream", $object["stream"]);
+
+		if(isset($object["value"]))
+			$retval[] = $object["value"];
+
+	$retval[] = "endobj";
+
+	return(implode("\n", $retval));
+	}
+
+################################################################################
+# _pdf_glue_object ( string $value ) : string
+# returns $value as string (escaped-format).
+################################################################################
+
+function _pdf_glue_string($value)
+	{
+	$value = iconv("UTF-8", "ISO-8859-1", $value);
+
+	$value = str_replace(array("\\", "(", ")"), array("\\\\", "\(", "\)"), $value);
+
+	return($value);
+	}
+  ?>
