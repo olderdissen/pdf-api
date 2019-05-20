42806e17772712c467dfcc0bc8429a00a7232bb3
olderdissen <50807808+olderdissen@users.noreply.github.com>
2019-05-20 06:50:08 +0200

    Create comment.php

---
+++ parse/comment.php
@@ -0,0 +1,29 @@
+################################################################################
+# _pdf_parse_comment ( string $data ) : array
+# returns array of found element as string and unparsed data as string.
+################################################################################
+
+function _pdf_parse_comment($data)
+	{
+	$retval = "";
+
+	while(1)
+		{
+		if(strlen($data) == 0)
+			break;
+		elseif(in_array($data[0], array("\n", "\r")))
+			break;
+		elseif($data[0] == "\\")
+			{
+			$retval .= $data[0];
+
+			$data = substr($data, 1);
+			}
+
+		$retval .= $data[0];
+
+		$data = substr($data, 1);
+		}
+
+	return(array($retval, $data));
+	}
