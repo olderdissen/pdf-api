a94b725a0986319ac1545b1c5a56a41de031450c
olderdissen <50807808+olderdissen@users.noreply.github.com>
2019-05-20 06:47:35 +0200

    Create string.php

---
+++ parse/string.php
@@ -0,0 +1,29 @@
+################################################################################
+# _pdf_parse_string ( string $data ) : array
+# returns array of found element as string and unparsed data as string.
+################################################################################
+
+function _pdf_parse_string($data)
+	{
+	$retval = "";
+
+	while(1)
+		{
+		if(strlen($data) == 0)
+			die("_pdf_parse_string: process runs out of data.");
+		elseif($data[0] == ")")
+			break;
+		elseif($data[0] == "\\")
+			{
+			$retval .= $data[0];
+
+			$data = substr($data, 1);
+			}
+
+		$retval .= $data[0];
+
+		$data = substr($data, 1);
+		}
+
+	return(array($retval, $data));
+	}
